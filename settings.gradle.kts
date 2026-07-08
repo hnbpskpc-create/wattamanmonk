edd521420c038aa19c1e30eb0db3b2e7a2a3c72e
Meng Horl <hnbpskpc@gmail.com>
2026-07-08 16:03:06 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ស្រង់វត្តមានវត្ត"
+include(":app")
